556fe5c99301995fde8de92253148cc1041a4118
BlockSplitME <evgeniy.belov.12@mail.ru>
2023-01-12 19:49:36 +0300

    f

--- src/scripts/menuScript.js
+++ src/scripts/menuScript.js
@@ -3,12 +3,13 @@ function createMenu() {
 }
 function changeHref(){
   let path = "/testSite/src/templates/";
-  document.getElementById("perception").href = path + "1_perception/";
-  document.getElementById("singing").href    = path + "2_singing/";
-  document.getElementById("musicActiv").href = path + "3_musicActiv/";
-  document.getElementById("gamesDMI").href   = path + "4_gamesDMI.html";
-  document.getElementById("gamesActiv").href = path + "5_gamesActiv.html";
-  document.getElementById("quest").href      = path + "6_quest.html";
-  document.getElementById("archive").href    = path + "7_archive.html";
-  document.getElementById("contact").href    = path + "8_contact.html";
+  document.getElementById("main").href       =        "/testSite/";
+  document.getElementById("perception").href = path + "perception/";
+  document.getElementById("singing").href    = path + "singing/";
+  document.getElementById("musicActiv").href = path + "musicActiv/";
+  document.getElementById("gamesDMI").href   = path + "gamesDMI/";
+  document.getElementById("gamesActiv").href = path + "gamesActiv/";
+  document.getElementById("quest").href      = path + "questioning/";
+  document.getElementById("archive").href    = path + "archive/";
+  document.getElementById("contact").href    = path + "contact/";
 }
