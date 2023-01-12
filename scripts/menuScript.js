3e739c59263810f27549f2335bf1d5594ae09cfd
BlockSplitME <79277777+BlockSplitME@users.noreply.github.com>
2023-01-12 14:05:41 +0300

    Update menuScript.js

--- scripts/menuScript.js
+++ scripts/menuScript.js
@@ -2,12 +2,13 @@ function createMenu() {
 
 }
 function changeHref(){
-  document.getElementById("precept").href    = "1_precept.html";
-  document.getElementById("singing").href    = "2_singing.html";
-  document.getElementById("musicActiv").href = "3_musicActiv.html";
-  document.getElementById("gamesDMI").href   = "4_gamesDMI.html";
-  document.getElementById("gamesActiv").href = "5_gamesActiv.html";
-  document.getElementById("quest").href      = "6_quest.html";
-  document.getElementById("archive").href    = "7_archive.html";
-  document.getElementById("contact").href    = "8_contact.html";
+  let path = "https://blocksplitme.github.io/testSite/templates/";
+  document.getElementById("precept").href    = path + "1_precept.html";
+  document.getElementById("singing").href    = path + "2_singing.html";
+  document.getElementById("musicActiv").href = path + "3_musicActiv.html";
+  document.getElementById("gamesDMI").href   = path + "4_gamesDMI.html";
+  document.getElementById("gamesActiv").href = path + "5_gamesActiv.html";
+  document.getElementById("quest").href      = path + "6_quest.html";
+  document.getElementById("archive").href    = path + "7_archive.html";
+  document.getElementById("contact").href    = path + "8_contact.html";
 }
