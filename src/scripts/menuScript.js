66db6064f77f901f6f718d4e79386106cfbf6de8
BlockSplitME <79277777+BlockSplitME@users.noreply.github.com>
2023-01-12 14:52:20 +0300

    Add files via upload

---
+++ src/scripts/menuScript.js
@@ -0,0 +1,14 @@
+function createMenu() {
+
+}
+function changeHref(){
+  let path = "https://blocksplitme.github.io/testSite/src/templates/";
+  document.getElementById("precept").href    = path + "1_precept/";
+  document.getElementById("singing").href    = path + "2_singing/";
+  document.getElementById("musicActiv").href = path + "3_musicActiv/";
+  document.getElementById("gamesDMI").href   = path + "4_gamesDMI.html";
+  document.getElementById("gamesActiv").href = path + "5_gamesActiv.html";
+  document.getElementById("quest").href      = path + "6_quest.html";
+  document.getElementById("archive").href    = path + "7_archive.html";
+  document.getElementById("contact").href    = path + "8_contact.html";
+}
