4d0e089913737a7d21863a35ceafa90ee4807587
BlockSplitME <79277777+BlockSplitME@users.noreply.github.com>
2023-01-12 13:10:25 +0300

    Add files via upload

---
+++ scripts/menuScript.js
@@ -0,0 +1,13 @@
+function createMenu() {
+
+}
+function changeHref(){
+  document.getElementById("precept").href    = "1_precept.html";
+  document.getElementById("singing").href    = "2_singing.html";
+  document.getElementById("musicActiv").href = "3_musicActiv.html";
+  document.getElementById("gamesDMI").href   = "4_gamesDMI.html";
+  document.getElementById("gamesActiv").href = "5_gamesActiv.html";
+  document.getElementById("quest").href      = "6_quest.html";
+  document.getElementById("archive").href    = "7_archive.html";
+  document.getElementById("contact").href    = "8_contact.html";
+}
