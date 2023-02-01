50a06fc5885a3238b4a8c0337ced5665890751a6
BlockSplitME <evgeniy.belov.12@mail.ru>
2023-02-01 13:37:41 +0300

    f

--- src/scripts/menuScript.js
+++ src/scripts/menuScript.js
@@ -1,6 +1,3 @@
-function createMenu() {
-
-}
 function changeHref(){
   let path = "/testSite/src/templates/";
   document.getElementById("main").href       =        "/testSite/";
@@ -9,7 +6,23 @@ function changeHref(){
   document.getElementById("musicActiv").href = path + "musicActiv/";
   document.getElementById("gamesDMI").href   = path + "gamesDMI/";
   document.getElementById("gamesActiv").href = path + "gamesActiv/";
-  document.getElementById("quest").href      = path + "questioning/";
+  document.getElementById("quest").href      = "";
   document.getElementById("archive").href    = path + "archive/";
-  document.getElementById("contact").href    = path + "contact/";
+  document.getElementById("contact").href    = "";
+
+}
+
+function openMenuList() {
+  document.querySelector('.menuList').style.display = 'block';
+  if(document.querySelector('#menuCheckBox').checked == true) {
+    document.querySelector('#menuCheckBox').checked = false;
+    document.querySelector('#imgMenu').src = "../../../src/pict/menu.png";
+    document.querySelector('.menuList').style.display = "none";
+  } else {
+    document.querySelector('#menuCheckBox').checked = true;
+    document.querySelector('#imgMenu').src = "../../../src/pict/close.png";
+    document.querySelector('.menuList').style.display = "block";
+  
+  }
+  console.log(document.querySelector('#menuCheckBox').checked);
 }
